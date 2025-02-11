6c6937d0801e6333cee12cbd7b3e984f9f6f4da3
Berkay Kılıç <167620810+kberkaykilic@users.noreply.github.com>
2025-02-11 17:54:54 +0300

    Update Super_Lig.py

--- Super_Lig.py
+++ Super_Lig.py
@@ -1,13 +1,13 @@
 import random
 
-class Clubs:
+class SuperLig:
     teams = ["Galatasaray", "Fenerbahçe", "Samsunspor", "Eyüpspor", "Göztepe",
              "Başakşehir", "Beşiktaş", "Kasımpaşa", "Alanyaspor", "Rizespor",
              "Trabzonspor", "Gaziantep FK", "Antalyaspor", "Konyaspor", "Sivasspor",
              "Kayserispor", "Bodrumspor", "Hatayspor", "Adana Demirspor"]
 
     def __init__(self):
-        self.teams = Clubs.teams[:]
+        self.teams = SuperLig.teams[:]
         if len(self.teams) % 2 == 1:
             self.teams.append("Bay")
 
@@ -28,15 +28,15 @@ class Clubs:
             self.points[away] += 1
 
     def set_fixture(self):
-        num_weeks = len(self.teams) - 1
+        num_weeks = (len(self.teams) - 1) * 2
         fixture = {}
 
         teams = self.teams[:]
         random.shuffle(teams)
 
-        for week in range(1, num_weeks + 1):
+        # First Leg
+        for week in range(1, len(self.teams)):
             matches = []
-
             for i in range(len(teams) // 2):
                 home = teams[i]
                 away = teams[-(i + 1)]
@@ -47,7 +47,21 @@ class Clubs:
                     matches.append(f"{home} {home_score} - {away_score} {away}")
 
             fixture[week] = matches
+            teams = [teams[0]] + [teams[-1]] + teams[1:-1]
 
+        # Revenge Leg
+        for week in range(len(self.teams), num_weeks + 1):
+            matches = []
+            for i in range(len(teams) // 2):
+                home = teams[i]
+                away = teams[-(i + 1)]
+
+                if home != "Bay" and away != "Bay":
+                    away_score, home_score = self.simulate_match(home, away)
+                    self.update_points(home, away, home_score, away_score)
+                    matches.append(f"{away} {away_score} - {home_score} {home}")
+
+            fixture[week] = matches
             teams = [teams[0]] + [teams[-1]] + teams[1:-1]
 
         return fixture
@@ -68,6 +82,13 @@ class Clubs:
             print(f"{rank}. {team}: {points} points")
         print("-" * 30)
 
-clubs = Clubs()
-clubs.print_fixture()
-clubs.print_standings()
+    def print_champion(self):
+        champion = sorted(self.points.items(), key=lambda x: x[1], reverse=True)
+        top_team, top_points = champion[0]
+        print(f"Champion: {top_team} with {top_points} points")
+
+
+super_lig = SuperLig()
+super_lig.print_fixture()
+super_lig.print_standings()
+super_lig.print_champion()
